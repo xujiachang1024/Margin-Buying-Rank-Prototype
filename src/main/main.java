785e446d19bf4ac889144d4c817636ecef7ff5c2
Jiachang Xu <jiachanx@usc.edu>
2017-07-15 10:12:43 +0800

    Add Selenium WebDriver

--- src/main/main.java
+++ src/main/main.java
@@ -2,18 +2,12 @@ package main;
 
 import java.util.List;
 
-import back.EastMoneyParserJsoup;
 import back.MarginSecurity;
 
 public class main {
 
 	public static void main(String [] args) {
-		EastMoneyParserJsoup parser = new EastMoneyParserJsoup();
-		List<MarginSecurity> shanghai = parser.updateShanghai();
-		for(MarginSecurity sh : shanghai) {
-			System.out.println(sh.getCode() + " " + sh.getName() + " " + sh.getNetBuyingRatio());
-		}
-		System.out.println("end");
+		
 	}
 	
 }
